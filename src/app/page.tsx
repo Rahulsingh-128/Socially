42989ed2644386733eef0f89910da182b8731200
Rahul <rahulbuddies007@gmail.com>
2025-04-09 17:26:36 +0530

    image through imagekit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+import React from "react";
+import { IKImage } from "imagekitio-next";
+const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
-  )
-}
+    <>
+      <div className="">Homepage</div>;
+      <IKImage
+        urlEndpoint={urlEndpoint}
+        path="general/post.jpeg"
+        width={600}
+        height={600}
+        alt="test post"
+      />
+    </>
+  );
+};
 
-export default Homepage
+export default Homepage;
--- src/components/LeftBar.tsx
+++ src/components/LeftBar.tsx
@@ -1,25 +1,34 @@
-import Image from "next/image";
+// import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-
+import Image from "./Image";
 const LeftBar = () => {
   return (
     <div>
       <h1>Left Side</h1>
       <div className=" overflow-hidden  mb-8">
-        <Image
+        {/* <Image
           src="/general/avatar.png"
           alt="rahul singh"
           width={80}
           height={80}
           className=" object-cover rounded-full"
-        />
+        /> */}
+        <div className="w-10 h-10 relative rounded-full overflow-hidden">
+          <Image
+            path="/general/avatar.png"
+            alt="lama dev"
+            w={100}
+            h={100}
+            tr={true}
+          />
+        </div>
 
         <Link
           href="/compose/post"
           className="bg-white text-black mt-8 rounded-full w-48 h-48 flex items-center justify-center xxl:hidden"
         >
-          <Image src="icons/post.svg" alt="new post" width={48} height={48} />
+          <Image path="icons/post.svg" alt="new post" w={48} h={48} />
         </Link>
         <Link
           href="/compose/post"
